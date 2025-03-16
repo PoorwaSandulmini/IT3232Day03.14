7ebd9636aefad2494418e8be8ef8a613ffe14eb5
PoorwaSandulmini <139644198+PoorwaSandulmini@users.noreply.github.com>
2025-03-16 21:57:52 +0530

    Question 2
    Print the numbers.

---
+++ Question2.java
@@ -0,0 +1,8 @@
+public class Question2{
+    public static void main(String[] args){
+        System.out.println("Print the numbers:");
+        for (int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
